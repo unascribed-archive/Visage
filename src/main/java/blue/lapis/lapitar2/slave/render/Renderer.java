c1c916fc2caa22235fb5c594ee16a9b3c61a6c1e
Aesen Vismea <aesen@gameminers.com>
2015-04-07 04:15:28 -0400

    Remove ibo and the color vbo

--- src/main/java/blue/lapis/lapitar2/slave/render/Renderer.java
+++ src/main/java/blue/lapis/lapitar2/slave/render/Renderer.java
@@ -1,6 +1,5 @@
 package blue.lapis.lapitar2.slave.render;
 
-import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.util.List;
@@ -21,81 +20,41 @@ import static org.lwjgl.opengl.GL11.*;
 
 public abstract class Renderer {
 	protected static final float vertices[] = {
+		// Front
 		-1.0f, -1.0f,  1.0f,
 		 1.0f, -1.0f,  1.0f,
+		 1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		// Back
+		-1.0f, -1.0f, -1.0f,
+		 1.0f, -1.0f, -1.0f,
+		 1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+		// Top
 		-1.0f,  1.0f,  1.0f,
 		 1.0f,  1.0f,  1.0f,
-
+		 1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+		// Bottom
+		-1.0f, -1.0f, -1.0f,
+		 1.0f, -1.0f, -1.0f,
+		 1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+		// Left
 		 1.0f, -1.0f,  1.0f,
 		 1.0f, -1.0f, -1.0f,
+		 1.0f,  1.0f, -1.0f,
 		 1.0f,  1.0f,  1.0f,
-		 1.0f,  1.0f, -1.0f,
-
-		 1.0f, -1.0f, -1.0f,
-		-1.0f, -1.0f, -1.0f,
-		 1.0f,  1.0f, -1.0f,
-		-1.0f,  1.0f, -1.0f,
-
+		// Right
 		-1.0f, -1.0f, -1.0f,
 		-1.0f, -1.0f,  1.0f,
-		-1.0f,  1.0f, -1.0f,
 		-1.0f,  1.0f,  1.0f,
-
-		-1.0f, -1.0f, -1.0f,
-		 1.0f, -1.0f, -1.0f,
-		-1.0f, -1.0f,  1.0f,
-		 1.0f, -1.0f,  1.0f,
-
-		-1.0f,  1.0f,  1.0f,
-		 1.0f,  1.0f,  1.0f,
-		-1.0f,  1.0f, -1.0f,
-		 1.0f,  1.0f, -1.0f,
-	};
-	
-	protected static final float colors[] = {
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-                               
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-                               
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-                               
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-                               
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-                               
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-	};
-	
-	protected static final byte indices[] = {
-		 0,  1,  3,     0,  3,  2,
-		 4,  5,  7,     4,  7,  6,
-		 8,  9, 11,     8, 11, 10,
-		12, 13, 15,    12, 15, 14,
-		16, 17, 19,    16, 19, 18,
-		20, 21, 23,    20, 23, 22,
+		-1.0f,  1.0f, -1.0f
 	};
 	protected final String name = getClass().getSimpleName();
 	protected Pbuffer pbuffer;
 	protected List<Cube> cubes = Lists.newArrayList();
-	protected int vbo, cvbo, ibo, texture;
+	protected int vbo, texture;
 	private boolean initialized = false;
 	protected void addCube(Cube cube) {
 		cubes.add(cube);
@@ -116,12 +75,12 @@ public abstract class Renderer {
 			pbuffer.destroy();
 			pbuffer = null;
 		}
-		vbo = cvbo = ibo = texture = 0;
+		vbo = texture = 0;
 		cubes.clear();
 		initialized = false;
 	}
 	public void init(int supersampling) throws LWJGLException {
-		Lapitar.log.info("["+name+"] Initializing Pbuffer (assuming "+supersampling+"x supersampling)");
+		Lapitar.log.finer("["+name+"] Initializing Pbuffer (assuming "+supersampling+"x supersampling)");
 		if (pbuffer != null) {
 			destroy();
 		}
@@ -129,12 +88,10 @@ public abstract class Renderer {
 		if (pbuffer.isBufferLost())
 			throw new LWJGLException("Could not create Pbuffer");
 		pbuffer.makeCurrent();
-		Lapitar.log.info("["+name+"] Setting up VBOs");
-		IntBuffer ids = BufferUtils.createIntBuffer(3);
+		Lapitar.log.finer("["+name+"] Setting up VBOs");
+		IntBuffer ids = BufferUtils.createIntBuffer(1);
 		glGenBuffersARB(ids);
 		vbo = ids.get();
-		cvbo = ids.get();
-		ibo = ids.get();
 		Util.checkGLError();
 		
 		FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertices.length);
@@ -142,20 +99,8 @@ public abstract class Renderer {
 		glBindBufferARB(GL_ARRAY_BUFFER_ARB, vbo);
 		glBufferDataARB(GL_ARRAY_BUFFER_ARB, vertexBuffer, GL_STATIC_DRAW_ARB);
 		Util.checkGLError();
-
-		FloatBuffer colorBuffer = BufferUtils.createFloatBuffer(colors.length);
-		colorBuffer.put(colors);
-		glBindBufferARB(GL_ARRAY_BUFFER_ARB, cvbo);
-		glBufferDataARB(GL_ARRAY_BUFFER_ARB, colorBuffer, GL_STATIC_DRAW_ARB);
-		Util.checkGLError();
 		
-		ByteBuffer indexBuffer = BufferUtils.createByteBuffer(indices.length);
-		indexBuffer.put(indices);
-		glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB, ibo);
-		glBufferDataARB(GL_ELEMENT_ARRAY_BUFFER_ARB, indexBuffer, GL_STATIC_DRAW_ARB);
-		Util.checkGLError();
-		
-		Lapitar.log.info("["+name+"] Initializing cubes");
+		Lapitar.log.finer("["+name+"] Initializing cubes");
 		initCubes();
 		initialized = true;
 	}
