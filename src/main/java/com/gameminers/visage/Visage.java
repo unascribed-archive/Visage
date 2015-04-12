dd468c9161f16d514f0ba78ed30fbe453b353204
Aesen Vismea <aesen@gameminers.com>
2015-04-12 15:14:39 -0400

    Fix version number

--- src/main/java/com/gameminers/visage/Visage.java
+++ src/main/java/com/gameminers/visage/Visage.java
@@ -20,7 +20,7 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 public class Visage {
-	public static final String VERSION = "2.0.0";
+	public static final String VERSION = "1.0.0";
 	public static final Formatter logFormat = new VisageFormatter();
 	public static final Logger log = Logger.getLogger("com.gameminers.visage");
 	public static boolean ansi;
